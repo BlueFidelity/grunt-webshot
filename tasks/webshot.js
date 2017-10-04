91b05191577ee08fd57929e91c37ca1951ba6451
Brandon Ouellette <brandon@bluefidelity.com>
2017-10-04 01:34:21 -0400

    added retry option on timeout

--- tasks/webshot.js
+++ tasks/webshot.js
@@ -12,7 +12,8 @@ module.exports = function (grunt) {
 	grunt.registerMultiTask('webshot', 'Render screenshots of webpages', function() {
 		var options = this.options({
 			site: null,
-			savePath: null
+			savePath: null,
+			retryOptions: {}
 		});
 		var done = this.async();
 
@@ -31,23 +32,41 @@ module.exports = function (grunt) {
 		// copy own options
 		var site = options.site;
 		var savePath = options.savePath;
+		var retryOptions = options.retryOptions;
 
 		// clean from webshot's options
 		delete options.site;
 		delete options.savePath;
+		delete options.retryOptions;
+		
+		if (retryOptions.maxAttempts && typeof retryOptions.waitTime !== 'number') {
+			retryOptions.waitTime = 1000;
+		}
+		
+		var runWebshot = function(attempt) {
+			webshot(site, savePath, options, function(err) {
+				if (err) {
+					grunt.log.writeln('webshot JSException: ' + err);
+				}
+			}, function(err) {
+				if (err) {
+					grunt.log.writeln('webshot error:');
+					if (retryOptions.maxAttempts && attempt < retryOptions.maxAttempts && err == 'Error: PhantomJS did not respond within the given timeout setting.') {
+						grunt.log.writeln('webshot warning: last attempt failed');
+						setTimeout(function() {
+							runWebshot(attempt++);
+						}, attempt * retryOptions.waitTime);
+					} else {
+						grunt.fail.warn(err);
+						done(false);
+					}
+				} else {
+					done(true);
+				}
+			});
+		};
 
 		// lets go
-		webshot(site, savePath, options, function(err) {
-			if (err) {
-				grunt.log.writeln('webshot warn: ' + err);
-			}
-		}, function(err) {
-			if (err) {
-				grunt.log.writeln('webshot error:');
-				grunt.fail.warn(err);
-				done(false);
-			}
-			done(true);
-		});
+		runWebshot(1);
 	});
 };
